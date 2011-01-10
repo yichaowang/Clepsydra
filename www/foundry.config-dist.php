f23553d0c9d1bba76a6a83eea4d0bea0f91e56f4
YichaoWang <wenwwyccc@gmail.com>
2011-01-09 21:12:44 -0500

    recovered dist files

---
+++ www/foundry.config-dist.php
@@ -0,0 +1,12 @@
+<?php
+
+date_default_timezone_set('America/New_York');
+
+foundry_init(array(
+	'namespace'					=> 'clepsydra',
+	'application-root'			=> '/Users/Yichao/Sites/Clepsydra/application',
+	'private-path'				=> 'private',
+	'config-path'				=> 'config'	
+));
+
+?>
