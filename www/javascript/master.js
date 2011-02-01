056b42a7998140430aadf11ed6b006530487daf4
Ryan McBride <ryan@ryanpmcbride.com>
2011-02-01 15:10:13 -0500

    Working on javascript error handling

--- www/javascript/master.js
+++ www/javascript/master.js
@@ -1,16 +1,54 @@
 document.addEvent('domready', function() {
-	
-/*
- Handles the status message passed via GET-back
-*/
-if( window.location.search.length 
-	&& window.location.search.indexOf('message=') != -1 ) {
 
-	var data = new URI(window.location.href).get('data');
+	// Handles input text replacement
+	$$('.text-replace').each(function(el) {
+		if( el.get('type') == 'password' ) {
+			el.set('type', 'text');
+			el.store('isPass', true);
+		}
+		
+		el.addEvents({
+			'focus': function(e) {
+				if( this.get('default') == this.get('value') ) {
+					this.set('value', '');
+				}
+				if( this.retrieve('isPass') ) {
+					this.set('type', 'password');
+				}
+			},
+			'blur': function(e) {
+				if( !this.get('value') ) {
+					this.set('value', this.get('default'));
+	
+					if( this.retrieve('isPass') ) {
+						this.set('type', 'text');
+					}
+	
+				}
+			}
+		});
+	});
+
+	// Handles the status message passed via GET-back
+	if( window.location.search.length && window.location.search.indexOf('message=') != -1 ) {
+		
+		var data = new URI(window.location.href).get('data'),
+			msgStr = data.message.replace(/[^a-zA-Z0-9\.\!\?\:\-\\\/\*]/g, ' '),
+			msgType = data.type;
+			
+		// call message class method here...	
+	}
+	
+	
+
+	
+
+	/*
+var data = new URI(window.location.href).get('data');
 	var message = data.message.unencode(), 
 		   type = data.type.unencode();
 	
-	message = message.replace(/[^a-zA-Z0-9\.\!\?\:\-\\\/\*]/g, ' ');
+	var message = data.replace(/[^a-zA-Z0-9\.\!\?\:\-\\\/\*]/g, ' ');
 	
 	switch(type) {
 		case 'success':
@@ -29,38 +67,13 @@ if( window.location.search.length
 	
 	
 	setTimeout(function() {
-		Rose.ui.statusMessage.display(message, type);
+		alert(message);
 	}, 1000);
+*/
 	
-}
 
-$$('.text-replace').each(function(el) {
-	if( el.get('type') == 'password' ) {
-		el.set('type', 'text');
-		el.store('isPass', true);
-	}
-	
-	el.addEvents({
-		'focus': function(e) {
-			if( this.get('default') == this.get('value') ) {
-				this.set('value', '');
-			}
-			if( this.retrieve('isPass') ) {
-				this.set('type', 'password');
-			}
-		},
-		'blur': function(e) {
-			if( !this.get('value') ) {
-				this.set('value', this.get('default'));
 
-				if( this.retrieve('isPass') ) {
-					this.set('type', 'text');
-				}
 
-			}
-		}
-	});
-});
 
 
 
